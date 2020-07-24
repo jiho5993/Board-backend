8e06885c674225e20296698c6ea5da8988a68414
Pjh <jiho5993@naver.com>
2020-07-25 01:54:26 +0900

    Add read and write api

--- router/api_article.js
+++ router/api_article.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
 
 /**
@@ -9,19 +10,55 @@ var mysql_dbc = require('../config/mysql-config')();
 var connection = mysql_dbc.init();
 mysql_dbc.test(connection);
 
+/**
+ * get the Article_list
+ */
 router.get('/list', (req, res) => {
   const sql = `select * from article`;
-  connection.query(sql, (err, row, fld) => {
+  connection.query(sql, (err, rows, fld) => {
     if(!err) {
-      res.json(row);
+      console.log(rows);
+      res.json(rows);
     } else {
       console.log('list get ERR' + err);
     }
-  })
+  });
 });
 
-router.get('/about', (req, res) => {
-  res.send("about api page.");
+/**
+ * read the Article
+ */
+router.get('/read/:id', (req, res) => {
+  const articleNo = req.params.id;
+  const sql = `select * from article where article_no = ?`;
+  connection.query(sql, [articleNo], (err, rows, fld) => {
+    if(!err) {
+      console.log(rows);
+      res.json(rows);
+    } else {
+      console.log('read get ERR' + err);
+    }
+  });
+});
+
+/**
+ * write the Article
+ */
+router.post('/write', (req, res) => {
+  var result = {};
+  const { title, writer, content } = req.body;
+  const now = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+  const sql = `insert into article(title, writer, content, reg_date) values (?, ?, ?, ?)`;
+  connection.query(sql, [title, writer, content, now], (err, rows, fld) => {
+    if(!err) {
+      console.log("done");
+      result = { success: 1 };
+      res.json(result);
+    } else {
+      result = { success: 0, err: err };
+      res.json(result);
+    }
+  });
 });
 
 module.exports = router;
