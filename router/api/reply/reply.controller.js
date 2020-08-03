c2050b1a071f2e07ad8da8141143ae659b66ca8f
Pjh <jiho5993@naver.com>
2020-08-03 23:22:53 +0900

    Add reply count

--- router/api/reply/reply.controller.js
+++ router/api/reply/reply.controller.js
@@ -38,17 +38,28 @@ exports.write = (req, res) => {
  */
 exports.list = (req, res) => {
   const id = req.params.id;
-  const qr = `select nickname, content, reg_date from reply where article_no = ?`;
-  connection.query(qr, [id], (err, replyList) => {
-    if(err) {
+  const list_qr = `select nickname, content, reg_date from reply where article_no = ?`;
+  connection.query(list_qr, [id], (err_1, replyList) => {
+    if(err_1) {
       res.json({
         success: 0,
-        error: err
+        error: err_1
       });
     } else {
-      res.json({
-        success: 1,
-        reply: replyList
+      const cnt_qr = `select count(article_no) as count from reply where article_no = ?`;
+      connection.query(cnt_qr, [id], (err_2, cnt) => {
+        if(err_2) {
+          res.json({
+            success: 0,
+            error: err_2
+          });
+        } else {
+          res.json({
+            success: 1,
+            count: cnt[0].count,
+            reply: replyList
+          });
+        }
       });
     }
   });
